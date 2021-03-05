310c39479ae451bc0b7988b764ed297a2b935de4
Kayuemkhan <abdulkayuem007@gmail.com>
2021-03-06 00:03:42 +0600

    initial commit

---
+++ app/src/main/java/code/fortomorrow/easysharedpref/SharedPrefClass.java
@@ -0,0 +1,32 @@
+package code.fortomorrow.easysharedpref;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPrefClass {
+
+    public static final String SHARED_PREF_MAIN="shared_preference_main";
+    private static SharedPreferences mSharedPref;
+    public static final String NAME = "initial.name";
+
+    private SharedPrefClass()
+    {
+
+    }
+    public static void init(Context context)
+    {
+        if(mSharedPref == null)
+            mSharedPref = context.getSharedPreferences( NAME, Context.MODE_PRIVATE);
+    }
+
+    public static String read(String key, String defValue) {
+        return mSharedPref.getString(key, defValue);
+    }
+
+    public static void write(String key, String value) {
+        SharedPreferences.Editor prefsEditor = mSharedPref.edit();
+        prefsEditor.putString(key, value);
+        prefsEditor.commit();
+    }
+
+}
